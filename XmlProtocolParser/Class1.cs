88a8b22efad602d0c7e148199e19f4c6109dccc4
Wesley Adams <wesleyadams001@gmail.com>
2019-03-11 17:22:46 -0500

    added calc

--- XmlProtocolParser/Class1.cs
+++ XmlProtocolParser/Class1.cs
@@ -4,5 +4,11 @@ namespace XmlProtocolParser
 {
     public class Class1
     {
+        public int Calc(int x)
+        {
+            var val = 1;
+            return val;
+        }
+        
     }
 }
---
+++ XmlProtocolParser/KnuthMorrisPrattAlgorithm.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XmlProtocolParser
+{
+    class KnuthMorrisPrattAlgorithm
+    {
+    }
+}
