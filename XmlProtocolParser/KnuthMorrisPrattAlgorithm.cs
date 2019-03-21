00577ce1a9598752df2029dc239844afe8e496bf
Wesley Adams <wesleyadams001@gmail.com>
2019-03-21 12:19:16 -0500

    first addition of KMP

--- XmlProtocolParser/KnuthMorrisPrattAlgorithm.cs
+++ XmlProtocolParser/KnuthMorrisPrattAlgorithm.cs
@@ -6,5 +6,87 @@ namespace XmlProtocolParser
 {
     class KnuthMorrisPrattAlgorithm
     {
+       /// <summary>
+       /// Use implementation of KMP to find first occurance of a byte pattern 
+       /// </summary>
+       /// <param name="text"></param>
+       /// <param name="pattern"></param>
+       /// <returns></returns>
+        public List<int> KMPSearch(string text, string pattern)
+        {
+            int N = text.Length;
+            int M = pattern.Length;
+
+            if (N < M) return -1;
+            if (N == M && text == pattern) return 0;
+            if (M == 0) return 0;
+
+            int[] lpsArray = new int[M];
+            List<int> matchedIndex = new List<int>();
+
+            LongestPrefixSuffix(pattern, ref lpsArray);
+
+            int i = 0, j = 0;
+            while (i < N)
+            {
+                if (text[i] == pattern[j])
+                {
+                    i++;
+                    j++;
+                }
+
+                // match found at i-j
+                if (j == M)
+                {
+                    matchedIndex.Add(i - j);
+                    Console.WriteLine((i - j).ToString());
+                    j = lpsArray[j - 1];
+                }
+                else if (i < N && text[i] != pattern[j])
+                {
+                    if (j != 0)
+                    {
+                        j = lpsArray[j - 1];
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+            }
+
+            return matchedIndex;
+        }
+
+        public void LongestPrefixSuffix(string pattern, ref int[] lpsArray)
+        {
+            int M = pattern.Length;
+            int len = 0;
+            lpsArray[0] = 0;
+            int i = 1;
+
+            while (i < M)
+            {
+                if (pattern[i] == pattern[len])
+                {
+                    len++;
+                    lpsArray[i] = len;
+                    i++;
+                }
+                else
+                {
+                    if (len == 0)
+                    {
+                        lpsArray[i] = 0;
+                        i++;
+                    }
+                    else
+                    {
+                        len = lpsArray[len - 1];
+                    }
+                }
+            }
+        }
+        
     }
 }
